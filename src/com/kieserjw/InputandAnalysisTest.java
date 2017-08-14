5c317b0af4bf5cc1c57e9039e82c96b5c58acd88
kieserjw <kieserjw@TheBIGIdea>
2017-08-14 17:16:29 -0500

    renaming folder name

--- src/com/kieserjw/InputandAnalysisTest.java
+++ src/com/kieserjw/InputandAnalysisTest.java
@@ -10,7 +10,7 @@ public class InputandAnalysisTest {
 
     @Test
     public void empty_file() {
-        String inputFilename = "test/empty_file.txt";
+        String inputFilename = "input_and_analysis_test_files/empty_file.txt";
         InputandAnalysis iaa = new InputandAnalysis(inputFilename);
         assertEquals(iaa.getNumberList().size(), 0);
         assertEquals(iaa.getStringList().size(), 0);
@@ -19,7 +19,7 @@ public class InputandAnalysisTest {
 
     @Test
     public void just_numbers() {
-        String inputFilename = "test/just_numbers.txt";
+        String inputFilename = "input_and_analysis_test_files/just_numbers.txt";
         InputandAnalysis iaa = new InputandAnalysis(inputFilename);
         assertEquals(iaa.getNumberList().size(), 6);
         assertEquals(iaa.getMean(), 5.03333, 1E-5);
@@ -30,7 +30,7 @@ public class InputandAnalysisTest {
 
     @Test
     public void just_strings() {
-        String inputFilename = "test/just_strings.txt";
+        String inputFilename = "input_and_analysis_test_files/just_strings.txt";
         InputandAnalysis iaa = new InputandAnalysis(inputFilename);
         assertEquals(iaa.getStringList().size(), 10);
         String temp = iaa.getStringList().get(0);
@@ -40,7 +40,7 @@ public class InputandAnalysisTest {
 
     @Test
     public void numbers_and_strings() {
-        String inputFilename = "test/numbers_and_strings.txt";
+        String inputFilename = "input_and_analysis_test_files/numbers_and_strings.txt";
         InputandAnalysis iaa = new InputandAnalysis(inputFilename);
         assertEquals(iaa.getNumberList().size(), 4);
         assertEquals(iaa.getMean(), 5.55, 1E-5);
